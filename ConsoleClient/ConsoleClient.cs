e346c51e5fd648967662803676971693b6fbe924
kashin.aleksandr <kashin.aleksandr@itmh.ru>
2025-01-15 10:52:56 +0500

    Добавил вывод ошибок в консольный клиент

--- ConsoleClient/ConsoleClient.cs
+++ ConsoleClient/ConsoleClient.cs
@@ -9,13 +9,19 @@ public class ConsoleClient(IApp app) : IClient
 {
 	public void Run()
 	{
-		Parser.Default.ParseArguments<ConsoleSettings>(Environment.GetCommandLineArgs())
-			.WithParsed(settings => app.Run(
-				new TagCloud.Settings.Settings
+		try
+		{
+			Parser.Default.ParseArguments<ConsoleSettings>(Environment.GetCommandLineArgs())
+				.WithParsed(settings => app.Run(new TagCloud.Settings.Settings
 				{
 					AppSettings = settings.GetAppSettings(),
 					ImageSettings = settings.GetImageSettings()
 				}))
-			.WithNotParsed(_ => throw new ArgumentException("Invalid command line arguments"));
+				.WithNotParsed(_ => throw new ArgumentException("Invalid command line arguments"));
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine($"Во время выполнения программы возникла ошибка: {e.Message}");
+		}
 	}
 }
